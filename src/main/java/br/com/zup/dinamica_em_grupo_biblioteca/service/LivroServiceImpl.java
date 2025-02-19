44b47ca8e9749c45d02f03a19cb2579ffdecfa5f
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-02-19 14:57:54 -0300

    feat: Cria o endpoint listarTodos e refactor: inseri as annotations na classe LivroEntity

--- src/main/java/br/com/zup/dinamica_em_grupo_biblioteca/service/LivroServiceImpl.java
+++ src/main/java/br/com/zup/dinamica_em_grupo_biblioteca/service/LivroServiceImpl.java
@@ -1,8 +1,25 @@
 package br.com.zup.dinamica_em_grupo_biblioteca.service;
 
+import br.com.zup.dinamica_em_grupo_biblioteca.model.LivroEntity;
+import br.com.zup.dinamica_em_grupo_biblioteca.repositories.LivroRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LivroServiceImpl implements LivroService{
+
+    private final LivroRepository livroRepository;
+
+    public LivroServiceImpl(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    @Override
+    public List<LivroEntity> listarTodosLivros() {
+        List<LivroEntity> livro = livroRepository.findAll();
+        return livro;
+    }
+
 
 }
