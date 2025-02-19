ed64cff4ee74a49cdc688aaaac97fbeadd2f7c16
Bruni <bruniely.marchioro@zup.com.br>
2025-02-19 14:39:46 -0300

    remove import

--- src/main/java/br/com/zup/dinamica_em_grupo_biblioteca/repositories/LivroRepository.java
+++ src/main/java/br/com/zup/dinamica_em_grupo_biblioteca/repositories/LivroRepository.java
@@ -2,8 +2,6 @@ package br.com.zup.dinamica_em_grupo_biblioteca.repositories;
 
 
 
-import br.com.zup.dinamica_em_grupo_biblioteca.model.LivroEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<LivroEntity, Long> {
 
